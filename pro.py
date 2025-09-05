697d7dc06bf4b864b881f7d002bf29d8800758a0
M4R71Nii <martinvigh46@gmail.com>
2025-09-05 12:16:00 +0200

    done

--- pro.py
+++ pro.py
@@ -1,1 +1,28 @@
-print("Szia!")
+print ("Szia!")
+print ("Tibi")
+print ("Szia" ,"Tóth" , "Karcsi" , "!", sep="")
+print ("Szia Tóth Karcsi!")
+nev = "Tóth Karcsi"
+print(f"Szia {nev}!")
+print ("Szia", "Peti", sep="\n", end="!") #\t
+#adattpusok
+# int()
+# float()
+# str()
+# bool()
+# list()
+print()
+print(int("5"))
+print(float(5))
+print(str(5.0))
+print(type(str(5.0)))
+print(bool(-1))
+print(["hétfő", "kedd","szerda "])
+napok = ["hétfő" , "kedd" ,"szerda "]
+nevek =["Tibi" , "Sanyi" , "Tibi"]
+print(f"Napok: {set(nevek)}")
+print (tuple([1,2,3]))
+
+# HF: git parancsok:
+#git init : adott mappa verziókövetésének inditása
+#git add . 
